e9e5697df85db7c3b1b3f7b49e3d09f34d1d64ce
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2024-03-20 14:14:15 +0530

    NAvbar completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import "./App.css"
 import About from './components/about/About'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,60 +3,75 @@ import 'font-awesome/css/font-awesome.min.css';
 import "./header.css";
 
 const Header = () => {
-  const [Toggle, showMenu] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const [activeLink, setActiveLink] = useState('');
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  const handleSetActiveLink = (link) => {
+    if (link !== activeLink) {
+      setActiveLink(link);
+    } else {
+      setActiveLink('');
+    }
+  };
+
   return (
-    <header className="header">
-      <nav className="nav container">
-        <a href="index.html" className="nav__logo">
-          Anuraj
+    <header className="w-full fixed top-0 left-0 z-10 bg-white">
+      <nav className="container flex justify-between items-center gap-x-4 py-6">
+        <a href="index.html" className="flex text-4xl font-bold px-8">
+          <p>Anu</p>
+          <p className="text-red-600">raj</p>
         </a>
 
-        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
-          <ul className="nav__list grid">
-            <li className="nav__item">
-              <a href="#home" className="nav__link active-link">
+        <div className={`w-full justify-end mr-10 flex nav__menu bg-slate-100 md:bg-inherit ${toggle ? 'show-menu' : ''}`}>
+          <ul className="md:flex gap-10 md:text-xl md:font-semibold grid grid-cols-3 ">
+            <li className="">
+              <a href="#home" className={`nav__link ${activeLink === 'home' ? 'underline text-red-600' : ''}`} onClick={() => handleSetActiveLink('home')}>
                 <i className="uil uil-estate nav__icon"></i> Home
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#about" className="nav__link">
+              <a href="#about" className={`nav__link ${activeLink === 'about' ? 'underline text-red-600' : ''}`} onClick={() => handleSetActiveLink('about')}>
                 <i className="uil uil-user nav__icon"></i> About
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#skills" className="nav__link">
+              <a href="#skills" className={`nav__link ${activeLink === 'skills' ? 'underline text-red-600' : ''}`} onClick={() => handleSetActiveLink('skills')}>
                 <i className="uil uil-file-alt nav__icon"></i> Skills
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#services" className="nav__link">
+              <a href="#services" className={`nav__link ${activeLink === 'services' ? 'underline text-red-600' : ''}`} onClick={() => handleSetActiveLink('services')}>
                 <i className="uil uil-briefcase-alt nav__icon"></i> Services
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#Porfolio" className="nav__link">
+              <a href="#Porfolio" className={`nav__link ${activeLink === 'portfolio' ? 'underline text-red-600' : ''}`} onClick={() => handleSetActiveLink('portfolio')}>
                 <i className="uil uil-scenery nav__icon"></i> Portfolio
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#contact" className="nav__link">
+              <a href="#contact" className={`nav__link ${activeLink === 'contact' ? 'underline text-red-600' : ''}`} onClick={() => handleSetActiveLink('contact')}>
                 <i className="uil uil-message nav__icon"></i> Contact
               </a>
             </li>
           </ul>
 
           <i
-            className="uil uil-times nav__close"
-            onClick={() => showMenu(!Toggle)}
+            className="uil uil-times nav__close "
+            onClick={handleToggle}
           ></i>
         </div>
-        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
-          <i className="uil uil-apps"></i>
+        <div className="nav__toggle px-3" onClick={handleToggle}>
+          <i className={`uil uil-apps text-red-600 text-2xl ${toggle ? 'transform rotate-90 border-2 p-2 rounded-xl border-blue-300' : ''}`}></i>
         </div>
       </nav>
     </header>
